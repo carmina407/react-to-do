65404baf732ba7fd9f4e4d9ac3a87779559338ea
carmina407 <42854355+carmina407@users.noreply.github.com>
2019-03-23 22:29:22 -0400

    Add files via upload

---
+++ ToDo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+  render() {
+    return (
+      <li>Study</li>
+      <li>Sleep</li>
+    );
+  }
+}
+
+export default ToDo;
