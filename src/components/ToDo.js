af4ef6f57ba82fed469b5f5babee8d8a0a59a6d4
carmina407 <42854355+carmina407@users.noreply.github.com>
2019-03-27 22:38:54 -0400

    Update toDo.js props-and-state complete

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,10 +3,13 @@ import React, { Component } from 'react';
 class ToDo extends Component {
   render() {
     return (
-      <li>Study</li>
-      <li>Sleep</li>
+      <li>
+        <input type="checkbox" checked={ this.props.isCompleted } />
+        <span>{ this.props.description }</span>
+        </li>
     );
   }
 }
 
 export default ToDo;
+
