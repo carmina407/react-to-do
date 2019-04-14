cfe59be0c0711a8a51476f08724a9d072427ff98
carmina407 <42854355+carmina407@users.noreply.github.com>
2019-04-13 21:53:22 -0400

    Final revision

--- src/App.js
+++ src/App.js
@@ -2,65 +2,83 @@ import React, { Component } from 'react';
 import './App.css';
 import ToDo from './components/ToDo.js';
 
- class App extends Component {
-   constructor(props) {
-     super(props);
-     this.state = {
-       todos: [
-         { id: 1, description: 'Cook dinner' , isCompleted: true },
-         { id: 2, description: 'Do the dishes' , isCompleted: false },
-         { id: 3, description: 'Give the baby a bath' , isCompleted: false }
-       ]
-       newTodoDescription: ''
-     };
-     this.deleteTodo = this.deleteTodo.bind(this);
-   }
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-handleChange(e){
-  this.setState({ newTodoDescription: e.target.value })
-      }
+    this.state = {
+      todos: [
+        { id: 1, description: 'Cook dinner' , isCompleted: true },
+        { id: 2, description: 'Do the dishes' , isCompleted: false },
+        { id: 3, description: 'Give the baby a bath' , isCompleted: false }
+      ],
+      newTodoDescription: ''
+    };
 
-   handleSubmit(e){
-     e.preventDefault();
-          if (!this.state.newTodoDescription) { return }
-     console.log('handleSubmit called');
-     const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
-     this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
-       }
-
-toggleComplete(index){
-  const todos = this.state.todos.slice();
-  const todo = todos[index];
-  todo.isCompleted = todo.isCompleted ? false : true;
-  this.setState({ todos: todos });
-}
-
-deleteTodo(id){
-    this.setState((prevState) => ({
-      items: prevState.items.filter(item => item.id !== id),
-    }))
+    this.deleteTodo = this.deleteTodo.bind(this);
   }
 
-   render(){
-     return (
-       <div className="App">
+  handleChange(e){
+    this.setState({ newTodoDescription: e.target.value })
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+
+    if (!this.state.newTodoDescription) { return }
+
+    console.log('handleSubmit called');
+
+    const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+
+    this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
+  }
+
+  toggleComplete(index){
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+
+    todo.isCompleted = todo.isCompleted ? false : true;
+
+    this.setState({ todos });
+  }
+
+  deleteTodo(id){
+    const todos = this.state.todos.filter(todo => todo.id !== id)
+
+    this.setState({
+      todos
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
         <ul>
-    { this.state.todos.map( (todo, index) =>
-        <ToDo key={ index }
-         description={ todo.description }
-          isCompleted={ todo.isCompleted }
-          toggleComplete={ () => this.toggleComplete(index) }
-           onDelete={ this.deleteTodo }
-           />
-      )}
+          {
+            this.state.todos.map((todo, index) => (
+              <ToDo
+                key={ index }
+                description={ todo.description }
+                isCompleted={ todo.isCompleted }
+                toggleComplete={ () => this.toggleComplete(index) }
+                onDelete={ this.deleteTodo }
+                todoid={ todo.id }
+              />
+            ))
+          }
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
-             <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
-        <input type="submit" />
+          <input
+            type="text"
+            value={ this.state.newTodoDescription }
+            onChange={ (e) => this.handleChange(e) }
+          />
+          <input type="submit" />
         </form>
-     </div>
-     );
-   }
- }
+      </div>
+    );
+  }
+}
 
 export default App;
